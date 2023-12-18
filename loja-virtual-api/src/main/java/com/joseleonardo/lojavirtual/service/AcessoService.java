29a5450fe3a7ca77241aa52547dee46e08c91e18
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-12-18 17:25:40 -0300

    Criando o service da entidade Acesso

---
+++ loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/service/AcessoService.java
@@ -0,0 +1,14 @@
+package com.joseleonardo.lojavirtual.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joseleonardo.lojavirtual.repository.AcessoRepository;
+
+@Service
+public class AcessoService {
+
+	@Autowired
+	private AcessoRepository acessoRepository;
+
+}
