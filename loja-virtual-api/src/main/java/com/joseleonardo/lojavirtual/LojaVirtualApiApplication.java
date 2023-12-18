ae43e5ca607868e5a53ac3ddb4a89f115c69412a
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-12-17 21:16:56 -0300

    Criando o projeto

---
+++ loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/LojaVirtualApiApplication.java
@@ -0,0 +1,13 @@
+package com.joseleonardo.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualApiApplication.class, args);
+	}
+
+}
