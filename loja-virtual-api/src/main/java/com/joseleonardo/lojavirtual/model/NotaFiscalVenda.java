7f055cea8274d8822ce8a49299964a765178bc0f
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-12-18 02:09:31 -0300

    Atualizando a entidade NotaFiscalVenda para receber o campo de vendaCompraLojaVirtual

--- loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/model/NotaFiscalVenda.java
+++ loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/model/NotaFiscalVenda.java
@@ -12,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -40,6 +41,10 @@ public class NotaFiscalVenda implements Serializable {
 
 	@Column(columnDefinition = "text", nullable = false)
 	private String pdf;
+	
+	@OneToOne
+	@JoinColumn(name = "venda_compra_loja_virtual_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "fk_venda_compra_loja_virtual"))
+	private VendaCompraLojaVirtual vendaCompraLojaVirtual;
 	
 	@ManyToOne(targetEntity = PessoaJuridica.class)
 	@JoinColumn(name = "empresa_id", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "fk_empresa"))
@@ -91,6 +96,14 @@ public class NotaFiscalVenda implements Serializable {
 
 	public void setPdf(String pdf) {
 		this.pdf = pdf;
+	}
+
+	public VendaCompraLojaVirtual getVendaCompraLojaVirtual() {
+		return vendaCompraLojaVirtual;
+	}
+
+	public void setVendaCompraLojaVirtual(VendaCompraLojaVirtual vendaCompraLojaVirtual) {
+		this.vendaCompraLojaVirtual = vendaCompraLojaVirtual;
 	}
 
 	public PessoaJuridica getEmpresa() {
