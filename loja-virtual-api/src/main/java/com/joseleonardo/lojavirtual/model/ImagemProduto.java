f9992de74554530708572ef77c0cb8a917325e07
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-12-18 01:38:37 -0300

    Criando a entidade ImagemProduto

---
+++ loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/model/ImagemProduto.java
@@ -0,0 +1,101 @@
+package com.joseleonardo.lojavirtual.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "imagem_produto")
+@SequenceGenerator(name = "seq_imagem_produto", sequenceName = "seq_imagem_produto", allocationSize = 1, initialValue = 1)
+public class ImagemProduto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_imagem_produto")
+	private Long id;
+
+	@Column(columnDefinition = "text", nullable = false)
+	private String imagemOriginal;
+
+	@Column(columnDefinition = "text", nullable = false)
+	private String imagemMiniatura;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "fk_produto"))
+	private Produto produto;
+	
+	@ManyToOne(targetEntity = PessoaJuridica.class)
+	@JoinColumn(name = "empresa_id", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "fk_empresa"))
+	private PessoaJuridica empresa;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getImagemOriginal() {
+		return imagemOriginal;
+	}
+
+	public void setImagemOriginal(String imagemOriginal) {
+		this.imagemOriginal = imagemOriginal;
+	}
+
+	public String getImagemMiniatura() {
+		return imagemMiniatura;
+	}
+
+	public void setImagemMiniatura(String imagemMiniatura) {
+		this.imagemMiniatura = imagemMiniatura;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	public PessoaJuridica getEmpresa() {
+		return empresa;
+	}
+	
+	public void setEmpresa(PessoaJuridica empresa) {
+		this.empresa = empresa;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ImagemProduto other = (ImagemProduto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
