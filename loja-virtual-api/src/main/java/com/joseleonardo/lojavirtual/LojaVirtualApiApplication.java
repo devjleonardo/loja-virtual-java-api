0fa6cfdb38003b520307d956f068618422312e35
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-12-18 02:22:28 -0300

    Adicionando anotações na classe Application do Spring Boot

--- loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/LojaVirtualApiApplication.java
+++ loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/LojaVirtualApiApplication.java
@@ -2,8 +2,16 @@ package com.joseleonardo.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.joseleonardo.lojavirtual.model")
+@ComponentScan(basePackages = { "com.joseleonardo.*" })
+@EnableJpaRepositories(basePackages = { "com.joseleonardo.lojavirtual.repository" })
+@EnableTransactionManagement
 public class LojaVirtualApiApplication {
 
 	public static void main(String[] args) {
