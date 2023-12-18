61fb4e03db037cef24d233d56f8b15a47e37c538
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-12-17 21:24:05 -0300

    Criando a entidade Acesso e implementando o Spring Security nela

---
+++ loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/model/Acesso.java
@@ -0,0 +1,67 @@
+package com.joseleonardo.lojavirtual.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", allocationSize = 1, initialValue = 1)
+public class Acesso implements GrantedAuthority {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome; /* Acesso ex: ROLE_ADMIN ou ROLE_VENDEDOR" */
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String getAuthority() {
+		return nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Acesso other = (Acesso) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
