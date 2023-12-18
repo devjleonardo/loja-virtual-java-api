9dc37b5fa112ed74e6ab872420013b3f13fb8ccf
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-12-17 23:59:25 -0300

    Criando as entidades Usuario e UsuarioAcesso

---
+++ loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/model/Usuario.java
@@ -0,0 +1,193 @@
+package com.joseleonardo.lojavirtual.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "usuario")
+@SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario", allocationSize = 1, initialValue = 1)
+public class Usuario implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
+	private Long id;
+
+	@Column(nullable = false)
+	private String login;
+
+	@Column(nullable = false)
+	private String senha;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date dataAtualizacaoSenha;
+
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "fk_pessoa"))
+	private Pessoa pessoa;
+	
+	@ManyToOne(targetEntity = PessoaJuridica.class)
+	@JoinColumn(name = "empresa_id", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "fk_empresa"))
+	private PessoaJuridica empresa;
+
+	@OneToMany(fetch = FetchType.EAGER)
+	@JoinTable(
+			 name = "usuario_acesso",
+			 uniqueConstraints = @UniqueConstraint(
+					 columnNames = {"usuario_id", "acesso_id"},
+			         name = "unique_usuario_acesso"),
+
+			 joinColumns = @JoinColumn(name = "usuario_id",
+			 						   referencedColumnName = "id", 
+			 						   table = "usuario",
+			 						   unique = false,
+			 						   foreignKey = @ForeignKey(
+			 								   name = "fk_usuario", 
+			 								   value = ConstraintMode.CONSTRAINT)),
+
+			 inverseJoinColumns = @JoinColumn(name = "acesso_id",
+			                                  referencedColumnName = "id",
+			                                  table = "acesso",
+			                                  unique = false,
+			                                  foreignKey = @ForeignKey(
+			                                		  name = "fk_acesso",
+			                                          value = ConstraintMode.CONSTRAINT))
+			)
+	private List<Acesso> acessos = new ArrayList<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Date getDataAtualizacaoSenha() {
+		return dataAtualizacaoSenha;
+	}
+
+	public void setDataAtualizacaoSenha(Date dataAtualizacaoSenha) {
+		this.dataAtualizacaoSenha = dataAtualizacaoSenha;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public PessoaJuridica getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(PessoaJuridica empresa) {
+		this.empresa = empresa;
+	}
+
+	public List<Acesso> getAcessos() {
+		return acessos;
+	}
+
+	public void setAcessos(List<Acesso> acessos) {
+		this.acessos = acessos;
+	}
+
+	/*Autoridades = São os acessos, ou seja ROLE_ADMIN, ROLE_SECRETARIO, ROLE_FINANCEIRO*/
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return acessos;
+	}
+
+	@Override
+	public String getUsername() {
+		return login;
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
