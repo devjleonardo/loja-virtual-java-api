f49b831cefc0ea7b5be74bdbfbe5172a9516bf44
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-12-19 02:02:54 -0300

    Criando um método no JWTLoginFilter para retornar uma mensagem quando o usuário ou senha estiverem inválidos

--- loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/security/JWTLoginFilter.java
+++ loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/security/JWTLoginFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -51,5 +52,15 @@ public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
 			e.printStackTrace();
 		}
 	}
-
+	
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException failed) throws IOException, ServletException {
+		if (failed instanceof BadCredentialsException) {
+			response.getWriter().write("Usuário ou senha inválidos");
+		} else {
+			response.getWriter().write("Falha ao realizar o login: " + failed.getMessage());
+		}
+	}
+	
 }
