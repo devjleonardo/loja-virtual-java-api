e4e7e1e0210b69590c65217da69bfca8dc65ea37
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-12-19 01:50:55 -0300

    Criando o JWTApiAutenticacaoFilter que será o filtro onde todas as requisições serão capturadas para serem autenticadas

---
+++ loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/security/JWTApiAutenticacaoFilter.java
@@ -0,0 +1,32 @@
+package com.joseleonardo.lojavirtual.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+/* Filtro onde todas as requisições serão capturadas para serem autenticadas */
+public class JWTApiAutenticacaoFilter extends GenericFilterBean {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		/* Estabelecer a autenticação do usuário */
+		Authentication authentication = new JWTTokenAutenticacaoService()
+				.getAuthentication((HttpServletRequest) request, (HttpServletResponse) response);
+
+		/* Coloca o processo de autenticação para o Spring Security */
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		chain.doFilter(request, response);
+	}
+
+}
