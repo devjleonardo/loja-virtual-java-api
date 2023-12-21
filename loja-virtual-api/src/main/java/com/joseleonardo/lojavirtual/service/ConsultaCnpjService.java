bf5d207d1185533eba80f424fecc4ff779604763
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-12-21 06:52:26 -0300

    Criando controller para consultar CNPJ através da Receita WS e trazer os dados da pessoa jurídica de forma automática

---
+++ loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/service/ConsultaCnpjService.java
@@ -0,0 +1,17 @@
+package com.joseleonardo.lojavirtual.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.joseleonardo.lojavirtual.model.dto.cnpj.CnpjDTO;
+
+@Service
+public class ConsultaCnpjService {
+	
+	public CnpjDTO consultarCnpjReceitaWS(String cnpj) {
+		return new RestTemplate()
+				.getForEntity("https://receitaws.com.br/v1/cnpj/" + cnpj, CnpjDTO.class)
+				.getBody();
+	}
+
+}
