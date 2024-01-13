e1d5a83426191722d8cebea81a1a6d0a4cd0c7d4
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2024-01-13 19:29:30 -0300

    Criando objeto para criar nova cobrança através da integração com a API de pagamento da Asaas

---
+++ loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/api/integracao/pagamento/asaas/dto/AsaasPagamentoCobrancaDiscountDTO.java
@@ -0,0 +1,34 @@
+package com.joseleonardo.lojavirtual.api.integracao.pagamento.asaas.dto;
+
+import java.io.Serializable;
+
+public class AsaasPagamentoCobrancaDiscountDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	// Valor percentual ou fixo de desconto a ser aplicado sobre o valor da cobrança
+	private float value;
+
+	/*
+	 * Dias antes do vencimento para aplicar desconto. Ex: 0 = até o vencimento, 1 =
+	 * até um dia antes, 2 = até dois dias antes, e assim por diante
+	 */
+	private float dueDateLimitDays;
+
+	public float getValue() {
+		return value;
+	}
+
+	public void setValue(float value) {
+		this.value = value;
+	}
+
+	public float getDueDateLimitDays() {
+		return dueDateLimitDays;
+	}
+
+	public void setDueDateLimitDays(float dueDateLimitDays) {
+		this.dueDateLimitDays = dueDateLimitDays;
+	}
+
+}
---
+++ loja-virtual-api/src/main/java/com/joseleonardo/lojavirtual/api/integracao/pagamento/asaas/dto/AsaasPagamentoCobrancaInterestDTO.java
@@ -0,0 +1,20 @@
+package com.joseleonardo.lojavirtual.api.integracao.pagamento.asaas.dto;
+
+import java.io.Serializable;
+
+public class AsaasPagamentoCobrancaInterestDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	// Percentual de juros ao mês sobre o valor da cobrança para pagamento após o vencimento
+	private float value;
+
+	public float getValue() {
+		return value;
+	}
+
+	public void setValue(float value) {
+		this.value = value;
+	}
+
+}
